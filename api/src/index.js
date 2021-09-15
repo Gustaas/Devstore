2576a9a2a1df33f2b535b0293047296714813671
gusta <contatogustavo0743@gmail.com>
2021-09-15 20:40:32 -0300

    funfou?

---
+++ api/src/index.js
@@ -0,0 +1,85 @@
+import db from './db.js';
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/produto', async(req, resp) => {
+    try{
+        let produtos = await db.tb_produto.findAll({ order: [[ 'id_produto', 'desc' ]] });
+        resp.send(produtos)
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+})
+
+app.post('/produto', async(req, resp) => {
+    try{
+
+        let { nome, categoria, precode, precopor, avaliacao, descricao, qtdestoque, imagem } = req.body
+
+        let r = await db.tb_produto.create({
+            nm_produto: nome,
+            ds_categoria: categoria,
+            vl_preco_de: precode,
+            vl_preco_por: precopor,
+            vl_avaliacao: avaliacao,
+            ds_produto: descricao,
+            qtd_estoque: qtdestoque,
+            img_produto: imagem,
+            bt_ativo: 1,
+            dt_inclusao: new Date()
+        })
+        console.log(r);
+
+        resp.send(r)
+
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+})
+
+app.put('/produto/:id', async(req, resp) => {
+    try {
+        let { nome, categoria, precode, precopor, avaliacao, descricao, qtdestoque, imagem } = req.body
+        let { id } = req.params
+
+        let r = await db.tb_produto.update({
+            nm_produto: nome,
+            ds_categoria: categoria,
+            vl_preco_de: precode,
+            vl_preco_por: precopor,
+            vl_avaliacao: avaliacao,
+            ds_produto: descricao,
+            qtd_estoque: qtdestoque,
+            img_produto: imagem,
+            bt_ativo: 1,
+            dt_inclusao: new Date() 
+        },
+        {
+            where: { id_produto: id}
+        })
+
+        resp.sendStatus(200);
+
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+})
+
+app.delete('/produto/:id', async(req, resp) => {
+    try {
+        let {id} = req.params;
+
+        await db.tb_produto.destroy( {where: { id_produto: id } })
+
+        resp.sendStatus(200);
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+})
+
+app.listen(process.env.PORT, x => console.log(`Server up at port ${process.env.PORT}`))
---
+++ react/site/src/pages/alunos/index.js
@@ -0,0 +1,234 @@
+import Cabecalho from '../../components/cabecalho'
+
+import Menu from '../../components/menu'
+
+import { Container, Conteudo } from './styled'
+
+import { useState, useEffect, React, useRef } from 'react'
+
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import LoadingBar from 'react-top-loading-bar'
+
+import Api from '../../service/api.js'
+const api = new Api();
+
+
+export default function Index() {
+    const [produto, setProduto] = useState([]);
+    const [nome, setNome] = useState('');
+    const [categoria, setCategoria] = useState('');
+    const [avaliacao, setAvaliacao] = useState('');
+    const [precoDe, setPrecoDe] = useState('');
+    const [precoPor, setPrecoPor] = useState('');
+    const [estoque, setEstoque] = useState('');
+    const [img, setImg] = useState('');
+    const [desc, setDesc] = useState('');
+    const [idAlterando, setIdAlterando] = useState(0)
+    const loading = useRef();
+
+    async function listar () {
+
+        loading.current.continuousStart();
+        let r = await api.listar();
+        setProduto(r);
+        loading.current.complete();
+
+    }
+
+    useEffect(() => { 
+        listar();
+    },
+     [])
+
+    async function inserir () {
+        loading.current.continuousStart();
+        if(nome !== ('') && categoria !== ('') && avaliacao !== ('') && precoDe !== ('') && precoPor !== ('') && estoque !== ('') && img !== ('') && desc !== ('')){
+            if(idAlterando === 0){
+                let r = await api.inserir(nome, categoria, precoDe, precoPor, avaliacao, desc, estoque, img);
+                if(r.erro)
+                    toast.dark(r.erro)
+                else
+                    toast.dark('Produto inserido!')
+            } else {
+                let r = await api.alterando( idAlterando, nome, categoria, precoDe, precoPor, avaliacao, desc, estoque, img);
+                if (r.erro)
+                toast.dark(r.erro)
+                else
+                    toast.dark('Produto alterado!')
+            }
+            limparCampos();
+        } else (
+                toast.dark('Campos inválidos')
+        )
+        listar();
+        loading.current.complete()
+    }
+
+    function limparCampos () {
+        setNome('')
+        setCategoria('')
+        setPrecoDe('')
+        setPrecoPor('')
+        setAvaliacao('')
+        setDesc('')
+        setEstoque('')
+        setImg('')
+
+        setIdAlterando(0);
+    }
+    async function remover (id) {
+        loading.current.continuousStart();
+
+        confirmAlert({
+            title: 'Remover Produto',
+            message: `Tem certeza que deseja remover o produto ${id}?`,
+            buttons: [
+                {
+                    label: 'Sim, remover',
+                    onClick: async() => {
+                        let r = await api.remover(id)
+                        if(r.erro) {
+                            toast.dark(r.erro)
+                        } else {
+                            toast.dark('Produto removido!')
+                        }
+                        listar();
+                    }
+                },
+                {
+                    label: 'Não, Cancelar'}
+            ]
+        })
+        listar();
+
+        loading.current.complete()
+    }
+
+    async function editar (item) {
+        setNome(item.nm_produto);
+        setCategoria(item.ds_categoria);
+        setPrecoDe(item.vl_preco_de);
+        setPrecoPor(item.vl_preco_por);
+        setAvaliacao(item.vl_avaliacao);
+        setDesc(item.ds_produto);
+        setEstoque(item.qtd_estoque);
+        setImg(item.img_produto);
+        setIdAlterando(item.id_matricula);
+    }
+
+
+    return (
+
+        <Container>
+            <ToastContainer/>
+            <LoadingBar color="#10EAEA" ref={loading} />
+            <Menu />
+            <Conteudo>
+                <Cabecalho />
+                <div class="body-right-box">
+                    <div class="new-student-box">
+                        
+                        <div class="text-new-student">
+                            <div class="bar-new-student"></div>
+                            <div class="text-new-student"> {idAlterando === 0 ? "Novo Produto" : "Alterando Produto " + idAlterando}</div>
+                        </div>
+
+                        <div class="input-new-student"> 
+                            <div class='inputsman'>
+                                <div class='input-upper'> 
+                                    <div class="input-left">
+                                        <div class="agp-input"> 
+                                            <div class="name-student"> Nome: </div>  
+                                            <div class="input"> <input type="text" value={nome} onChange={e => setNome(e.target.value)}/> </div>  
+                                        </div> 
+                                        <div class="agp-input">
+                                            <div class="number-student"> Categoria: </div>  
+                                            <div class="input"> <input type="text" value={categoria} onChange={e => setCategoria(e.target.value)}/> </div> 
+                                        </div>
+                                        <div class="agp-input">
+                                            <div class="number-student"> Avaliação: </div>  
+                                            <div class="input"> <input type="text" value={avaliacao} onChange={e => setAvaliacao(e.target.value)}/> </div> 
+                                        </div>
+                                    </div>
+
+                                    <div class="input-right">
+                                        <div class="agp-input">
+                                            <div class="corse-student"> Preço de: </div>  
+                                            <div class="input"> <input type="text" value={precoDe} onChange={e => setPrecoDe(e.target.value)} /> </div>  
+                                        </div>
+                                        <div class="agp-input">
+                                            <div class="class-student"> Preço por: </div>  
+                                            <div class="input"> <input type="text" value={precoPor} onChange={e => setPrecoPor(e.target.value)}/> </div> 
+                                        </div>
+                                        <div class="agp-input">
+                                            <div class="class-student"> Estoque: </div>  
+                                            <div class="input"> <input type="text" value={estoque} onChange={e => setEstoque(e.target.value)}/> </div> 
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class='input-down'>
+                                    <div class="agp-input-img">
+                                        <div class="corse-student"> Imagem: </div>  
+                                        <div class="input2"> <input type="text" value={img} onChange={e => setImg(e.target.value)} /> </div>  
+                                    </div>
+                                    <div class="agp-input-desc">
+                                        <div class="class-student"> Descrição: </div>  
+                                        <div class="input"> <textarea type="text" value={desc} onChange={e => setDesc(e.target.value)}/> </div> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="button-create"> <button onClick={inserir}> {idAlterando === 0 ? "Cadastrar": "Alterar"} </button> </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="student-registered-box">
+                        <div class="row-bar"> 
+                            <div class="bar-new-student"> </div>
+                            <div class="text-registered-student"> Produtos Inseridos </div>
+                        </div>
+                    
+                        <table class ="table-user">
+                
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th> ID </th>
+                                    <th> Produto </th>
+                                    <th> Categoria </th>
+                                    <th> Preço </th>
+                                    <th> Estoque </th>
+                                    <th class="coluna-acao"> </th>
+                                    <th class="coluna-acao"> </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            {produto.map((item, i) =>
+                                
+                                <tr className={i % 2 === 0 ? "linha-alternada": ""}>
+                                    <td> <img src={item.img_produto} alt='' width='50px' height='50px' /></td>
+                                    <td> {item.id_produto} </td>
+                                    <td title={item.nm_produto}> {item.nm_produto != null && item.nm_produto.length >= 25 ? item.nm_produto.substr(0, 25) + '...' : item.nm_produto}</td>
+                                    <td> {item.ds_categoria} </td>
+                                    <td> {item.vl_preco_de} </td>
+                                    <td> {item.qtd_estoque} </td>
+                                    <td className="coluna-acao"> <button onClick={ () => editar(item)}> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
+                                    <td className="coluna-acao"> <button onClick={ () => remover(item.id_produto) }> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
+                                </tr>
+
+                            )}
+                            </tbody> 
+                        </table>
+                    </div>
+                </div>
+            </Conteudo>
+        </Container>
+    )
+}
